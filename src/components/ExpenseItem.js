91a68059a2c2e811f0dc3de232cf93f4a9a2d8e8
lekyvong09 <lekyvong09@gmail.com>
2023-07-04 19:06:23 +0700

    change to use different method - ignore

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,40 +3,39 @@ import './ExpenseItem.css';
 function ExpenseItem(props) {
 
     // console.log(props.expenses);
-    var expenseList2 =[];
-    props.expenses.forEach(expense => expenseList2.push(
-        <div className='expense-item' key={expense.id}>
-            <div>{expense.date.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h3>{expense.title}</h3>
-                <div className='expense-item__price'>$ {expense.amount}</div>
-            </div>
-        </div>)
-    );
+    // var expenseList2 =[];
+    // props.expenses.forEach(expense => expenseList2.push(
+    //     <div className='expense-item' key={expense.id}>
+    //         <div>{expense.date.toISOString()}</div>
+    //         <div className='expense-item__description'>
+    //             <h3>{expense.title}</h3>
+    //             <div className='expense-item__price'>$ {expense.amount}</div>
+    //         </div>
+    //     </div>)
+    // );
 
-    var expenseList = props.expenses.map(expense => 
-        <div className='expense-item' key={expense.id}>
-            <div>{expense.date.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h3>{expense.title}</h3>
-                <div className='expense-item__price'>$ {expense.amount}</div>
-            </div>
-        </div>
-    );
+    // var expenseList = props.expenses.map(expense => 
+    //     <div className='expense-item' key={expense.id}>
+    //         <div>{expense.date.toISOString()}</div>
+    //         <div className='expense-item__description'>
+    //             <h3>{expense.title}</h3>
+    //             <div className='expense-item__price'>$ {expense.amount}</div>
+    //         </div>
+    //     </div>
+    // );
 
     /// JSX
     return (
         <div>
-            {/* {expenseList} */}
-            {/* {props.expenses.map(expense => (<div className='expense-item' key={expense.id}>
+            {props.expenses.map(expense => (
+                <div className='expense-item' key={expense.id}>
                     <div>{expense.date.toISOString()}</div>
                     <div className='expense-item__description'>
                         <h3>{expense.title}</h3>
                         <div className='expense-item__price'>$ {expense.amount}</div>
                     </div>
-                </div>))
-            } */}
-            {expenseList2}
+                </div>
+            ))}
         </div>
         
         // <div className='expense-item'>
